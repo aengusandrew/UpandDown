ef80f8ba04ee6b6bce37ecf5ac871565c65ac324
Aengus <andrewam@clarkson.edu>
2025-01-02 16:21:55 -0500

    Removed ID from upndown.js to make separate ID class

--- upndown.js
+++ upndown.js
@@ -25,13 +25,3 @@ export default class UpandDown {
         let currentRound = numRounds;
     }
 }
-
-function generateId(length) {
-    let ID = '';
-
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for(let i = 0; i < length; i++) ID += chars.charAt(Math.floor(Math.random() * chars.length));
-
-    return ID;
-}
