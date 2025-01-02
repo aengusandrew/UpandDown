ef554352137c910eaa306e6028dc449a035c03a0
Aengus <andrewam@clarkson.edu>
2025-01-02 16:17:14 -0500

    Added some general member methods to UpandDown class

--- upndown.js
+++ upndown.js
@@ -4,10 +4,25 @@ export default class UpandDown {
     constructor(gameId = generateId(32)) {
         this.gameID = gameId;
         this.deck = new Deck();
+        this.players = new Array();
     }
 
     getID() {
         return this.gameID;
+    }
+
+    numPlayers() {
+        return this.players.length;
+    }
+
+    addPlayer(player) {
+        this.players.push(player);
+    }
+
+    start() {
+        let numRounds = Math.floor(this.deck.numCards / this.numPlayers());
+
+        let currentRound = numRounds;
     }
 }
 
