e8092bd906cc4c44c6afc75841da129ec0515ba0
Aengus <andrewam@clarkson.edu>
2025-01-02 15:40:08 -0500

    Fixed Deck.deal() method, now appears to work.

--- deck.js
+++ deck.js
@@ -1,5 +1,3 @@
-import Hand from "/hand.js"
-
 const SUITS = ["SPADES", "CLUBS", "HEARTS", "DIAMONDS"]
 const VALUES = [
     "A",
@@ -42,14 +40,30 @@ export default class Deck {
     deal(num_players, num_cards) {
         let dealout = new Array(num_players);
 
-        dealout.fill(new Hand());
-
+        for (let i = 0; i < num_players; i++) {
+            dealout[i] = new Hand();
+        }
+    
         for(let i = 0; i < num_cards; i++) {
             for(let j = 0; j < num_players; j++) {
                 dealout[j].addCard(this.cards.pop());
             }
         }
         return dealout;
+    }
+}
+
+class Hand {
+    constructor(new_cards = new Array()) {
+        this.hand_cards = new_cards;
+    }
+
+    addCard(card) {
+        this.hand_cards.push(card);
+    }
+
+    getCards() {
+        return this.hand_cards;
     }
 }
 
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 import Deck from "/deck.js"
-import Hand from "/hand.js"
+import Hand from "/deck.js"
 
 const deck = new Deck();
 
-console.log(deck.cards);
+console.log(deck);
 
 deck.shuffle()
 
-console.log(deck.cards)
+console.log(deck)
 
 const hand = new Hand();
 
 console.log(hand);
 
 console.log(deck.deal(5,2))
+
+console.log(deck)
