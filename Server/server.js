2e703e5e740d5949cb225ef15698c96f97918269
Aengus <andrewam@clarkson.edu>
2025-04-26 22:02:01 -0400

    Spelling errors

--- Server/server.js
+++ Server/server.js
@@ -14,10 +14,10 @@ io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
     socket.on('disconnect', () => {
-        console.log('A user dosconnected:', socket.id);
+        console.log('A user disconnected:', socket.id);
     })
 });
 
 server.listen(3000, () => {
-    console.log('Server is litening on http://localhost:3000');
+    console.log('Server is listening on http://localhost:3000');
 });
