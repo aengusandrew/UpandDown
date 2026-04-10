6c330dc33b20632ba167df05192d94edfe936a6e
Aengus <andrewam@clarkson.edu>
2026-04-10 16:11:14 -0400

    Cards are always flying to the first opponent, not necessarily the one that won the trick. Need to debug.

--- Client/client.js
+++ Client/client.js
@@ -486,7 +486,7 @@ function animateTrickToWinner(state, trickCards) {
 
     if(!trickCards.length) return;
 
-    const winnerID = state.currentTurn;
+    const winnerID = state.currentTurn; // TODO: figure out why the cards don't fly to the winner all the time
     const winnerEl = document.querySelector(`[data-player-id="${winnerID}"]`);
     if(!winnerEl) return;
 
